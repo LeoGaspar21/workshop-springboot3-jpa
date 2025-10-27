cb4b2d1ba1ee9fd90b19bfbf5ed2e149664362dd
leonardo <leonardogaspar2001@gmail.com>
2025-10-27 17:18:45 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -42,6 +42,8 @@ public class OrderItem implements Serializable{
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
+	
+	
 	public Product getproduct() {
 		return id.getProduct();
 	}
